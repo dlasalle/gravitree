94d793c6360c898ced4625f22980677bd940d79f
Dominique LaSalle <dominique@solidlake.com>
2018-08-16 07:06:34 -0700

    Update unit tests

--- src/test/SolarSystem_test.cpp
+++ src/test/SolarSystem_test.cpp
@@ -17,190 +17,190 @@ namespace gravitree
 
 UNITTEST(SolarSystem, GetSystemRelativeToSun)
 {
-  Body sun("sun", 1.9885e30);
+  Body sun(0, 1.9885e30);
 
   SolarSystem system(sun);
 
-  Body earth("earth", 5.97237e24);
+  Body earth(3, 5.97237e24);
   system.addBody(
       earth,
       Vector3D(0, 1.47095e11, 0),
       Vector3D(3.029e4, 0, 0),
-      "sun");
+      0);
 
-  Body moon("moon", 7.342e22);
+  Body moon(31, 7.342e22);
   system.addBody(
       moon,
       Vector3D(-3.626e8, 0, 0),
       Vector3D(0, -1.022e3, 0),
-      "earth");
+      3);
 
-  Body mars("mars", 6.4171e23);
+  Body mars(4, 6.4171e23);
   system.addBody(
       mars,
       Vector3D(2.067e11, 0, 0),
       Vector3D(0, -2.650e4, 0),
-      "sun");
+      0);
 
   std::vector<std::pair<Body const *, Vector3D>> list = \
-      system.getRelativeTo("sun");
+      system.getRelativeTo(0);
 
   testEqual(list.size(), 4U);
 
   for (const std::pair<Body const *, Vector3D>& pair : list) {
-    if (pair.first->name() == "sun") {
+    if (pair.first->id() == 0) {
       testEqual(pair.first->mass(), 1.9885e30);
       testEqual(pair.second.x(), 0.0);
       testEqual(pair.second.y(), 0.0);
       testEqual(pair.second.z(), 0.0);
-    } else if (pair.first->name() == "earth") {
+    } else if (pair.first->id() == 3) {
       testEqual(pair.first->mass(), 5.97237e24);
       testNearEqual(pair.second.x(), 0.0, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), 1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "moon") {
+    } else if (pair.first->id() == 31) {
       testEqual(pair.first->mass(), 7.342e22);
       testNearEqual(pair.second.x(), -3.626e8, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), 1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "mars") {
+    } else if (pair.first->id() == 4) {
       testEqual(pair.first->mass(), 6.4171e23);
       testNearEqual(pair.second.x(), 2.067e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), 0.0, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
     } else {
-      testFail() << "Unknown object: " << pair.first->name();
+      testFail() << "Unknown object: " << pair.first->id();
     }
   }
 }
 
 UNITTEST(SolarSystem, GetSystemRelativeToEarth)
 {
-  Body sun("sun", 1.9885e30);
+  Body sun(0, 1.9885e30);
 
   SolarSystem system(sun);
 
-  Body earth("earth", 5.97237e24);
+  Body earth(3, 5.97237e24);
   system.addBody(
       earth,
       Vector3D(0, 1.47095e11, 0),
       Vector3D(3.029e4, 0, 0),
-      "sun");
+      0);
 
-  Body moon("moon", 7.342e22);
+  Body moon(31, 7.342e22);
   system.addBody(
       moon,
       Vector3D(-3.626e8, 0, 0),
       Vector3D(0, -1.022e3, 0),
-      "earth");
+      3);
 
-  Body mars("mars", 6.4171e23);
+  Body mars(4, 6.4171e23);
   system.addBody(
       mars,
       Vector3D(2.067e11, 0, 0),
       Vector3D(0, -2.650e4, 0),
-      "sun");
+      0);
 
-  Body phobos("phobos", 1.0659e16);
+  Body phobos(41, 1.0659e16);
   system.addBody(
       phobos,
       Vector3D(9.51758e6, 0, 0),
       Vector3D(0, -2.138e3, 0),
-      "mars");
+      4);
 
 
   std::vector<std::pair<Body const *, Vector3D>> list = \
-      system.getRelativeTo("earth");
+      system.getRelativeTo(3);
 
   testEqual(list.size(), 5U);
 
   for (const std::pair<Body const *, Vector3D>& pair : list) {
-    if (pair.first->name() == "sun") {
+    if (pair.first->id() == 0) {
       testEqual(pair.first->mass(), 1.9885e30);
       testNearEqual(pair.second.x(), 0.0, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), -1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "earth") {
+    } else if (pair.first->id() == 3) {
       testEqual(pair.first->mass(), 5.97237e24);
       testEqual(pair.second.x(), 0.0);
       testEqual(pair.second.y(), 0.0);
       testEqual(pair.second.z(), 0.0);
-    } else if (pair.first->name() == "moon") {
+    } else if (pair.first->id() == 31) {
       testEqual(pair.first->mass(), 7.342e22);
       testNearEqual(pair.second.x(), -3.626e8, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), 0.0, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "mars") {
+    } else if (pair.first->id() == 4) {
       testEqual(pair.first->mass(), 6.4171e23);
       testNearEqual(pair.second.x(), 2.067e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), -1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "phobos") {
+    } else if (pair.first->id() == 41) {
       testEqual(pair.first->mass(), 1.0659e16);
       testNearEqual(pair.second.x(), 2.0670951758e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), -1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
     } else {
-      testFail() << "Unknown object: " << pair.first->name();
+      testFail() << "Unknown object: " << pair.first->id();
     }
   }
 }
 
 UNITTEST(SolarSystem, GetSystemRelativeToMoon)
 {
-  Body sun("sun", 1.9885e30);
+  Body sun(0, 1.9885e30);
 
   SolarSystem system(sun);
 
-  Body earth("earth", 5.97237e24);
+  Body earth(3, 5.97237e24);
   system.addBody(
       earth,
       Vector3D(0, 1.47095e11, 0),
       Vector3D(3.029e4, 0, 0),
-      "sun");
+      0);
 
-  Body moon("moon", 7.342e22);
+  Body moon(31, 7.342e22);
   system.addBody(
       moon,
       Vector3D(-3.626e8, 0, 0),
       Vector3D(0, -1.022e3, 0),
-      "earth");
+      3);
 
-  Body mars("mars", 6.4171e23);
+  Body mars(4, 6.4171e23);
   system.addBody(
       mars,
       Vector3D(2.067e11, 0, 0),
       Vector3D(0, -2.650e4, 0),
-      "sun");
+      0);
 
   std::vector<std::pair<Body const *, Vector3D>> list = \
-      system.getRelativeTo("moon");
+      system.getRelativeTo(31);
 
   testEqual(list.size(), 4U);
 
   for (const std::pair<Body const *, Vector3D>& pair : list) {
-    if (pair.first->name() == "sun") {
+    if (pair.first->id() == 0) {
       testEqual(pair.first->mass(), 1.9885e30);
       testNearEqual(pair.second.x(), 3.626e8, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), -1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "earth") {
+    } else if (pair.first->id() == 3) {
       testEqual(pair.first->mass(), 5.97237e24);
       testNearEqual(pair.second.x(), 3.626e8, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), 0.0, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
-    } else if (pair.first->name() == "moon") {
+    } else if (pair.first->id() == 31) {
       testEqual(pair.first->mass(), 7.342e22);
       testEqual(pair.second.x(), 0.0);
       testEqual(pair.second.y(), 0.0);
       testEqual(pair.second.z(), 0.0);
-    } else if (pair.first->name() == "mars") {
+    } else if (pair.first->id() == 4) {
       testEqual(pair.first->mass(), 6.4171e23);
       testNearEqual(pair.second.x(), 2.070626e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.y(), -1.47095e11, 1.0e-3, 1.0);
       testNearEqual(pair.second.z(), 0.0, 1.0e-3, 1.0);
     } else {
-      testFail() << "Unknown object: " << pair.first->name();
+      testFail() << "Unknown object: " << pair.first->id();
     }
   }
 }
--- src/test/gravitree_test.cpp
+++ src/test/gravitree_test.cpp
@@ -15,33 +15,33 @@ UNITTEST(gravitree, usage)
   // all of the classes are tested in their respective unit tests, so this
   // test is to ensure everything complies fine and using the 'gravitree.hpp'
   // include works
-  gt::Body sun("sun", 1.9885e30);
+  gt::Body sun(1337, 1.9885e30);
 
   gt::SolarSystem system(sun);
 
-  gt::Body earth("earth", 5.97237e24);
+  gt::Body earth(3, 5.97237e24);
   system.addBody(
       earth,
       gt::Vector3D(0, 1.47095e11, 0),
       gt::Vector3D(3.029e4, 0, 0),
-      "sun");
+      1337);
 
-  gt::Body moon("moon", 7.342e22);
+  gt::Body moon(432, 7.342e22);
   system.addBody(
       moon,
       gt::Vector3D(-3.626e8, 0, 0),
       gt::Vector3D(0, -1.022e3, 0),
-      "earth");
+      3);
 
-  gt::Body mars("mars", 6.4171e23);
+  gt::Body mars(8731, 6.4171e23);
   system.addBody(
       mars,
       gt::Vector3D(2.067e11, 0, 0),
       gt::Vector3D(0, -2.650e4, 0),
-      "sun");
+      1337);
 
   std::vector<std::pair<gt::Body const *, gt::Vector3D>> list = \
-      system.getRelativeTo("sun");
+      system.getRelativeTo(1337);
 
   testEqual(list.size(), 4U);
 }
